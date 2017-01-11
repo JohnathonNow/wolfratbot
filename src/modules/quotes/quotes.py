6fa54bb340804632a1a3d010e82e196dec428acd
John Westhoff <JohnJWesthoff@gmail.com>
2017-01-10 19:50:32 -0500

    Added 'chance' field to quotes module
    This allows different probabilities to exist for different quotes.
    *shrug* for instance, always sends a gif in the current configuration

--- src/modules/quotes/quotes.py
+++ src/modules/quotes/quotes.py
@@ -4,11 +4,14 @@ import random
 QUOTES = '/home/john/wolfratbot/src/modules/quotes/quotes.yaml'
 
 def handler(SENDER, TEXT, send):
-    if random.randint(1,3) == 1:
-        quotes = yaml.load(file(QUOTES,'r'))
-        for quote in quotes:
-            for trigger in quote['triggers']:
-                if trigger.lower() in TEXT.lower():
+    quotes = yaml.load(file(QUOTES,'r'))
+    for quote in quotes:
+        for trigger in quote['triggers']:
+            if trigger.lower() in TEXT.lower():
+                chance = 33
+                if 'chance' in quote:
+                    chance = quote['chance']
+                if random.randint(0,100) <= chance:
                     if 'image' in quote:
                         send.sendImage(quote['image'],quote.get('quote',''))
                     else:
